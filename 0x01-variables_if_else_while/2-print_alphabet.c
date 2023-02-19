abeffa586a0f34f0a53b4b771a66ca1025093aca
fardosa001 <fardosaadow@gmail.com>
2023-02-19 05:45:59 -0800

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,11 @@ int main(void)
  * Return: Always 0.
  */
 {
-char letter;
-for (letter = 'a' ; letter <= 'z' ; letter++)
-putchar	(letter);
-putchar ('\n')
-return (0);	
+	char letter;
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	putchar	(letter);
+
+	putchar ('\n');
+
+	return (0);	
 }	 
