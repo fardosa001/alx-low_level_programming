e12feacad802351836e385dac4e55ada9c09a34f
fardosa001 <fardosaadow@gmail.com>
2023-02-19 00:13:48 -0800

    Possitive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,11 @@
 #include <stdlib.h>
 
 #include <time.h>
-<<<<<<< HEAD
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-=======
 
 /**main-Entry point
 *  
@@ -17,34 +15,26 @@
 *
 *Description: get ramdon number and print
 */
-<<<<<<< HEAD
 
-=======
->>>>>>> ed18deb96fed357da8a76a018f1a9485084069a5
->>>>>>> 367c3c44de91965be6bdfb4c67c39688e578c790
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-<<<<<<< HEAD
-=======
 	
->>>>>>> ed18deb96fed357da8a76a018f1a9485084069a5
-
-	if (n > 0)
+       if (n>0)
 
 		printf("%i is positive\n", n);
+                                        
+	else if (n==0)
 
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
+		printf("%i is zero\n", n); 
+                                           
+	else if 
+                
+		(printf("%i is negative\n", n));
+              
 
 	return (0);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> ed18deb96fed357da8a76a018f1a9485084069a5
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 variables,if else,while
-=======
-Variables If,else, While
->>>>>>> ed18deb96fed357da8a76a018f1a9485084069a5
