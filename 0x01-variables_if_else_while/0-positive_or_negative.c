0b8db00b1166547d2f384f5e87e18476415d35bc
fardosa001 <fardosaadow@gmail.com>
2023-02-18 23:26:12 -0800

    Possitive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 
 /**main-Entry point
+*  
 *Return: Always 0
+*
 *Description: get ramdon number and print
 */
+
 int main(void)
 {
 	int n;
@@ -15,11 +20,13 @@ int main(void)
 	
 
 	if (n > 0)
-		printf("%d is positive\n", n);
+
+		printf("%i is positive\n", n);
+
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 	else
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 
 	return (0);
 }
