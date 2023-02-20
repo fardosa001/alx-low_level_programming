a5c27cc9b2b5fedaa1075a9d5ec8eea12fdb3049
fardosa001 <fardosaadow@gmail.com>
2023-02-20 12:04:06 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print alphabet in lowercase except 'e' and 'q'
+ * Return: always 0
+ */
+int main(void)
+
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 102 || n == 113)
+		{
+			n++;
+		}
+		putchar (n);
+		n++;
+	}
+		putchar ('\n');
+
+		return (0);
+}
