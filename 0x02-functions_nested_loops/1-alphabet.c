7f868d1ff679e7384f6df1909248a7c048e2e9f2
fardosa001 <fardosaadow@gmail.com>
2023-02-21 07:11:35 -0800

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Check the code
+ * Description: print alphabet in lowercase
+ * Return 0
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++);
+	{
+		_putchar (n);
+	}
+		_putchar ('\n');
+	return (0);
+}
+
