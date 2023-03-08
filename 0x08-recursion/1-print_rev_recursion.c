565689d91735dff2772ee9eea704862694aa3250
fardosa001 <fardosaadow@gmail.com>
2023-03-08 12:06:08 -0800

    Reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,13 @@
   */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		return;
 	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
 	_putchar(*s);
 }
