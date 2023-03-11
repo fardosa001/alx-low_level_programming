3d257ae5b0e21111c0197c70fb38393d5943d756
fardosa001 <fardosaadow@gmail.com>
2023-03-11 04:41:49 -0800

    file that writes its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - - Print file name followed by new line.
@@ -12,3 +13,5 @@ int main(int argc, char *argv[])
 	printf("%s\n", argv[0]);
 	return (0);
 }
+
+
