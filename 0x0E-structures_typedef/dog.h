8f9f69e925c6a0a516c975accfb959a305d6bf9e
fardosa001 <fardosaadow@gmail.com>
2023-03-20 07:47:10 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - dog characteristics
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
