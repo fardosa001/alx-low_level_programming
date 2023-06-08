e6566919a2d99392e14f353874abb76f8834b86d
fardosa001 <fardosaadow@gmail.com>
2023-06-08 11:58:00 -0700

    finding factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *factorial - finds factorial of a given number
+  *@n: number whose factorial is to be found
+  *Return: integer value
+  */
+
+int factorial(int n)
+{
+	if (n < 1)
+	return (-1);
+
+	if (n <= 0)
+	return (1);
+
+	return (n * factorial(n - 1));
+}
+
