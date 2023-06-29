f7c062ffa3325b744d99b38e79456a4ede654725
fardosa001 <fardosaadow@gmail.com>
2023-06-29 03:29:10 -0700

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,20 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 /**
- *
+ * struct type - struct type
+ * @c: char data type
+ * @f: function
+ */
+
+typedef struct type
+{
+	char c;
+	void(*f)(va_list);
+} data_type;
+
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
 #endif
