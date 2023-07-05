d8b6f5f5f61b67de28c0a0643f0a492b4dd84dff
fardosa001 <fardosaadow@gmail.com>
2023-07-05 02:04:54 -0700

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - sums of all the data (n) of a listint_t linked list.
  * @head: head of the linked list
  * Return: 0 if the list is empty
  */
