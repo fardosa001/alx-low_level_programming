6e851680c6975dc07b1365323505c45090093db1
fardosa001 <fardosaadow@gmail.com>
2023-07-05 05:27:38 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *temp;
-	size_t i = 1;
+	size_t i = 0;
 
 	if (head == NULL)
 		return (NULL);
@@ -31,17 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		new_node->n = n;
 		*head = new_node;
 		return (new_node);
 	}
 	temp = *head;
-	while (i < idx)
+	while (i < idx - 1)
 	{
 	temp = temp->next;
 	i++;
 	}
-	new_node->n = n;
+
 	new_node->next = temp->next;
 	temp->next = new_node;
 	{
