712584ff10b875499e3a59b5ccbcdc1f130dc6ad
fardosa001 <fardosaadow@gmail.com>
2023-07-11 05:40:26 -0700

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument vector
+ * Return:0
+ */
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(av[1], av[2]);
+		exit(0);
+}
+/**
+ * copy_file - copies content of a file to another file.
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ * Return: copied file
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int f_fd, t_fd;
+	char *buff[1024];
+	int read_n;
+
+	f_fd = open(file_from, O_RDONLY);
+	if (!file_from || f_fd == -1)
+	{
+	dprintf(STDERR_FILENO, "ERROR: can't read from file %s/n", file_from);
+	exit(98);
+	}
+
+
+	t_fd = open(file_from, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((read_n = read(f_fd, buff, 1024)) > 0)
+	{
+		if (write(t_fd, buff, read_n) != read_n || t_fd == -1)
+		{
+
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_to);
+		       exit(99);
+		}
+	}
+
+	if (read_n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s/n", file_from);
+		exit(98);
+	}
+
+	if (close(f_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", f_fd);
+		exit(100);
+	}
+
+	if (close(t_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", t_fd);
+		exit(100);
+	}
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
+void copy_file(const char *file_from, const char *file_to);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
