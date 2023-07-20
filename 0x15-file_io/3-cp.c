344f3dfc89379bc164170d25fce1fa126f7cb742
fardosa001 <fardosaadow@gmail.com>
2023-07-20 08:39:20 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,73 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdarg.h>
+
 /**
- * main - Entry point
+ * error_handler - handles errors for cp
+ * @exit_code: exit code
+ * @m: message
+ * @type: data type
+ */
+
+void error_handler(int exit_code, char *m, char type, ...)
+{
+	va_list args;
+
+	va_start(args, type);
+	if (type == 's')
+		dprintf(STDERR_FILENO, m, va_arg(args, char *));
+	else if (type == 'd')
+		dprintf(STDERR_FILENO, m, va_arg(args, int));
+	else if (type == 'n')
+		dprintf(STDERR_FILENO, m, "");
+	else
+		dprintf(STDERR_FILENO, "Error: does not match any type\n");
+	va_end(args);
+	exit(exit_code);
+}
+
+/**
+ * main- copies content of a file to another file.
  * @ac: argument count
  * @av: argument vector
- * Return:0
+ * Return: 0
  */
-int main(int ac, char **av)
-{
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
 
-	copy_file(av[1], av[2]);
-	exit(0);
-}
-/**
- * copy_file - copies content of a file to another file.
- * @file_from: file to copy from
- * @file_to: file to copy to
- * Return: 0 (success), 97-100 (exit value errors)
- */
-void copy_file(const char *file_from, const char *file_to)
+int main(int ac, char *av[])
 {
 	int f_fd, t_fd;
 	char buff[1024];
-	int read_n;
+	ssize_t n_read, n_written;
 
-	f_fd = open(file_from, O_RDONLY);
-	if (!file_from || f_fd == -1)
+	if (ac != 3)
+		error_handler(97, "Usage: cp file_from file_to\n", 'n');
+
+	f_fd = open(av[1], O_RDONLY);
+	if (f_fd == -1)
+
+		error_handler(98, "Error: can't read from file %s\n", 's', av[1]);
+
+
+	t_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (t_fd == -1)
+		error_handler(99, "Error: can't write to %s\n", 's', av[2]);
+
+	while ((n_read = read(f_fd, buff, 1024)) > 0)
 	{
-	dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
-	exit(98);
+
+		n_written = write(t_fd, buff, n_read);
+		if (n_written == -1)
+		error_handler(99, "Error: can't write to %s\n", 's', av[2]);
 	}
 
-	t_fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((read_n = read(f_fd, buff, 1024)) > 0)
-	{
+	if (n_read == -1)
 
-		if (write(t_fd, buff, read_n) != read_n || t_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file_to);
-		       exit(99);
-		}
-	}
-	if (read_n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
-		exit(98);
-	}
-
+		error_handler(98, "Error: can't read from file %s\n", 's', av[1]);
 	if (close(f_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", f_fd);
-		exit(100);
-	}
+		error_handler(100, "Error: can't close fd %d\n", 'd', f_fd);
+
 	if (close(t_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", t_fd);
-		exit(100);
-	}
+		error_handler(100, "Error: can't close fd %d\n", 'd', t_fd);
+
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,8 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void copy_file(const char *file_from, const char *file_to);
+int main(int ac, char *av[]);
+void error_handler(int exit_code, char *m, char type, ...);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
