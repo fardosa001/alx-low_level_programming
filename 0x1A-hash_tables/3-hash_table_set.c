b0180288efc888f4214418a36989e0f86ebd56c9
fardosa001 <fardosaadow@gmail.com>
2023-09-08 13:12:47 -0700

    del ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *head;
+	hash_node_t *node, *head;
 	unsigned long int idx;
 
 	if (ht == NULL)
@@ -17,28 +17,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	idx = key_index((const unsigned char *) key, ht->size);
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
 
 	if (ht->array[idx] == NULL)
 	{
-		ht->array[idx] = new_node;
+		ht->array[idx] = node;
 	}
-
-	head = ht->array[idx];
-	if (strcmp(key, head->key) == 0)
-	head->value = strdup(value);
-
 	else
 	{
-	new_node->next = head;
-	ht->array[idx] = new_node;
-	}
+		head = ht->array[idx];
+		if (strcmp(key, head->key) == 0)
+		head->value = strdup(value);
 
+		else
+		{
+		node->next = head;
+		ht->array[idx] = node;
+		}
+	}
 	return (1);
 }
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int idx;
+
+	if (ht == NULL)
+		return;
+	idx = 0;
+	while (idx < ht->size)
+	{
+		node = ht->array[idx];
+		while (node != NULL)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+		idx++;
+	}
+	free(ht->array);
+	free(ht);
+}
