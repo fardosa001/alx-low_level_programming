05e660cd13047ca6fd0829e76ff9211ea2839a4a
fardosa001 <fardosaadow@gmail.com>
2023-09-13 05:40:55 -0700

    fix memory

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 	idx = 0;
-	while (idx < ht->size)
+	for  (idx = 0; idx < ht->size; idx++)
 	{
 		node = ht->array[idx];
 		while (node != NULL)
@@ -21,14 +21,11 @@ void hash_table_delete(hash_table_t *ht)
 			free(node->key);
 			free(node->value);
 			free(node);
-			if (temp != NULL)
-				node = temp;
-			else
-				break;
+			node = temp;
 
 		}
-		idx++;
 	}
+	
 	free(ht->array);
 	free(ht);
 }
